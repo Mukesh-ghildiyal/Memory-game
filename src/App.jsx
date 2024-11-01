cb7cb545987286ae0bc34aa1ab7c22b18a9f80b3
Mukesh-ghildiyal <115055244+Mukesh-ghildiyal@users.noreply.github.com>
2024-11-01 12:46:17 +0530

    memory game created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Memory from './components/Memory'
+
+const App = () => {
+  return (
+    <div className='text-3xl font-bold'>
+      <Memory />
+    </div>
+  )
+}
+
+export default App
